867b030bea463f2598cd6b88b1c29f2edb7083c7
altafsyah <syahrastanialtaf@gmail.com>
2024-06-25 16:35:51 +0700

    fix: fix temporary for eslint

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable react/no-unescaped-entities */
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
